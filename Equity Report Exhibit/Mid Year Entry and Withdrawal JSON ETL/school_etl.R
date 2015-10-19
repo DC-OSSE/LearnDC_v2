e87d9dd23d57666ea2cb78d88074853c1be78298
aaditya-madala <aaditya.madala@gmail.com>
2015-10-19 11:18:33 -0400

    Code: Rounding school and state MYE/MYW metrics to the tenth's place.

--- Equity Report Exhibit/Mid Year Entry and Withdrawal JSON ETL/school_etl.R
+++ Equity Report Exhibit/Mid Year Entry and Withdrawal JSON ETL/school_etl.R
@@ -4,20 +4,6 @@ library(jsonlite)
 library(reshape2)
 library(dplyr)
 
-move <- sqlQuery(dbrepcard_prod, "SELECT [School_Code], [School_Year], [Student_Group], [Month],[Metric], [NSize], [SchoolScore], [AverageScore] FROM [dbo].[equity_longitudinal3] WHERE [Metric] in ('Entry','Withdrawal','Net Cumulative')")
-move <- subset(move, move$School_Code!=1119 & move$School_Code!=216 & move$School_Code!=104 & move$School_Code !=137 & move$School_Code!=168 & move$School_Code!=128 & move$School_Code!=462 & move$School_Code!=456)
-move_long <- melt(move, id.vars = c("School_Year", "School_Code", "Student_Group","Month", "Metric"))
-move_long$Metric <- paste0(move_long$Metric, "_",move_long$variable)
-move_long$variable <- NULL
-move_wide <- dcast(move_long, School_Year + School_Code + Student_Group + Month ~ Metric, value.var = "value")
-colnames(move_wide) <- c("year","school_code","subgroup","month","state_entry","entry_n","entry","state_net_cumulative","net_cumulative_n","net_cumulative","state_withdrawal","withdrawal_n","withdrawal")
-move_wide$entry_n <- NULL
-move_wide$net_cumulative_n <- NULL
-move_wide$withdrawal_n <- NULL
-move_wide$withdrawal <- round(move_wide$withdrawal,2)
-move_wide <- select(move_wide, school_code, year, month, entry, withdrawal, net_cumulative, state_entry, state_withdrawal, state_net_cumulative)
-move_wide$school_code <- sapply(move_wide$school_code, leadgr, 4)
-
 ##
 myew <- sqlQuery(dbrepcard_prod,"select * from equity_report_school_longitudinal where reported=1 and metric in('Withdrawal','Entry','Net Cumulative')") %>% mutate(school_code=sapply(school_code,leadgr,4),month=ifelse(month %in% 'October',10,ifelse(month %in% 'November',11,ifelse(month %in% 'December',12,ifelse(month %in% 'January',1,ifelse(month %in% 'February',2,ifelse(month %in% 'March',3,ifelse(month %in% 'April',4,ifelse(month %in% 'May',5,NA))))))))) %>% select(-(starts_with("lea")),-(starts_with("re")),-(school_year),-(enrollment),-(grade),-(school_name),-(nsize))
 move_long <- melt(myew,id.vars=c("year","school_code","subgroup","month","metric"))
@@ -25,12 +11,12 @@ move_long$metric <- paste0(move_long$metric,"_",move_long$variable)
 move_long$variable <- NULL
 move_wide <- dcast(move_long,year + school_code + subgroup + month ~ metric,value.var="value")
 colnames(move_wide) <- c("year","school_code","subgroup","month","state_entry","entry","state_net_cumulative","net_cumulative","state_withdrawal","withdrawal")
-move_wide$withdrawal <- round(move_wide$withdrawal,2)
-move_wide$state_withdrawal <- round(move_wide$state_withdrawal,2)
-move_wide$state_entry <- round(move_wide$state_entry,2)
-move_wide$entry <- round(move_wide$entry,2)
-move_wide$net_cumulative <- round(move_wide$net_cumulative,2)
-move_wide$state_net_cumulative <- round(move_wide$state_net_cumulative,2)
+move_wide$withdrawal <- round(move_wide$withdrawal,3)
+move_wide$state_withdrawal <- round(move_wide$state_withdrawal,3)
+move_wide$state_entry <- round(move_wide$state_entry,3)
+move_wide$entry <- round(move_wide$entry,3)
+move_wide$net_cumulative <- round(move_wide$net_cumulative,3)
+move_wide$state_net_cumulative <- round(move_wide$state_net_cumulative,3)
 mvmt <- select(move_wide,school_code,year,month,entry,withdrawal,net_cumulative,state_entry,state_withdrawal,state_net_cumulative)
 
 strtable(mvmt)
--- Equity Report Exhibit/Mid Year Entry and Withdrawal JSON ETL/state_etl.R
+++ Equity Report Exhibit/Mid Year Entry and Withdrawal JSON ETL/state_etl.R
@@ -4,48 +4,6 @@ library(jsonlite)
 library(reshape2)
 library(dplyr)
 
-# move <- sqlQuery(dbrepcard_prod, "SELECT [School_Code], [School_Year], [Student_Group], [Month],[Metric], [NSize], [SchoolScore], [AverageScore] FROM [dbo].[equity_longitudinal3] WHERE [Metric] in ('Entry','Withdrawal','Net Cumulative')")
-# move <- subset(move, move$School_Code!=1119 & move$School_Code!=216 & move$School_Code!=104 & move$School_Code !=137 & move$School_Code!=168 & move$School_Code!=128 & move$School_Code!=462 & move$School_Code!=456)
-
-# move_long <- melt(move, id.vars = c("School_Year", "School_Code", "Student_Group","Month", "Metric"))
-# move_long$Metric <- paste0(move_long$Metric, "_",move_long$variable)
-# move_long$variable <- NULL
-
-# move_wide <- dcast(move_long, School_Year + School_Code + Student_Group + Month ~ Metric, value.var = "value")
-# colnames(move_wide) <- c("year","school_code","subgroup","month","state_entry","entry_n","entry","state_net_cumulative","net_cumulative_n","net_cumulative","state_withdrawal","withdrawal_n","withdrawal")
-# move_wide$entry_n <- NULL
-# move_wide$net_cumulative_n <- NULL
-# move_wide$withdrawal_n <- NULL
-# move_wide$withdrawal <- round(move_wide$withdrawal,2)
-# move_wide <- select(move_wide, school_code, year, month, entry, withdrawal, net_cumulative, state_entry, state_withdrawal, state_net_cumulative)
-# move_wide$school_code <- sapply(move_wide$school_code, leadgr, 4)
-
-# state_vals <- move_wide[which(move_wide$school_code=='0161'),]
-# state_vals$entry <- state_vals$state_entry
-# state_vals$withdrawal <- state_vals$state_withdrawal
-# state_vals$net_cumulative <- state_vals$state_net_cumulative
-
-# state_move_all <- state_vals %>%
-#   mutate(population='All',state_entry=NA,state_withdrawal=NA,state_net_cumulative=NA) %>%
-#   select(year,month,population,entry,withdrawal,net_cumulative,
-#   	state_entry,state_withdrawal,state_net_cumulative)
-
-# state_move_gen <- state_vals %>%
-#   mutate(population='Gen',entry=round(entry/1.25,4),withdrawal=round(withdrawal/1.25,4),
-#   	net_cumulative=round(net_cumulative/1.25,4),
-#          state_entry=NA,state_withdrawal=NA,state_net_cumulative=NA) %>%
-#   select(year,month,population,entry,withdrawal,net_cumulative,
-#   	state_entry,state_withdrawal,state_net_cumulative)
-
-# state_move_alt <- state_vals %>%
-#   mutate(population='Alt',entry=round(entry/1.75,4),withdrawal=round(withdrawal/1.75,4),
-#          net_cumulative=round(net_cumulative/1.75,4),
-#          state_entry=NA,state_withdrawal=NA,state_net_cumulative=NA) %>%
-#   select(year,month,population,entry,withdrawal,net_cumulative,
-#   	state_entry,state_withdrawal,state_net_cumulative)
-
-# state_move <- rbind.data.frame(state_move_all,state_move_gen,state_move_alt)
-
 myew <- sqlQuery(dbrepcard_prod,"select * from equity_report_state_longitudinal where reported=1 and metric in ('Entry','Exit','Net Cumulative')") %>% mutate(month=ifelse(month %in% 'October',10,ifelse(month %in% 'November',11,ifelse(month %in% 'December',12,ifelse(month %in% 'January',1,ifelse(month %in% 'February',2,ifelse(month %in% 'March',3,ifelse(month %in% 'April',4,ifelse(month %in% 'May',5,NA)))))))),population='All') %>% arrange(month_order) %>% select(-starts_with("days"),-(school_year),-(grade),-(month_order),-starts_with("re"),-starts_with("days"),-(count),-(nsize),-(enrollment))
 
 move_long <- melt(myew,id.vars=c("year","subgroup","month","metric","population"))
@@ -53,11 +11,13 @@ move_long$metric <- paste0(move_long$metric,"_",move_long$variable)
 move_long$variable <- NULL
 move_wide <- dcast(move_long,year + subgroup + month + population ~ metric,value.var="value")
 colnames(move_wide) <- c("year","subgroup","month","population","entry","withdrawal","net_cumulative")
+move_wide$withdrawal <- round(move_wide$withdrawal,3)
+move_wide$entry <- round(move_wide$entry,3)
+move_wide$net_cumulative <- round(move_wide$net_cumulative,3)
 move_wide$state_entry <- NA
 move_wide$state_withdrawal <- NA
 move_wide$state_net_cumulative <- NA
 
-# move_wide$withdrawal <- round(move_wide$withdrawal,2)
 mvmt <- select(move_wide,year,month,population,entry,withdrawal,net_cumulative,state_entry,state_withdrawal,state_net_cumulative)
 
 strtable(mvmt)
