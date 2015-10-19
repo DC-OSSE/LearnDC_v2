DC-OSSE/LearnDC_v2

commit 2ac05c4b568fe0cf0bfe2f70f657c13fd0501717

Code: Rounding school and state suspension1+ metrics to the tenths place and long term suspensions to hundredths place.

Equity Report Exhibit/Suspension JSON ETL/school_etl.R
@@ -4,26 +4,6 @@ library(jsonlite)
 library(reshape2)
 library(dplyr)
 
-susp <- sqlQuery(dbrepcard_prod, "SELECT [School_Code], [School_Year], [Student_Group], [Metric], [NSize], [SchoolScore], [AverageScore]
-		FROM [dbo].[equity_longitudinal3] WHERE [Metric] in ('Suspended 1+','Suspended 11+','Total Suspensions')")
-susp <- subset(susp, !is.na(School_Code))
-susp_long <- melt(susp, id.vars = c("School_Year", "School_Code", "Student_Group", "Metric"))
-susp_long$Metric<- paste0(susp_long$Metric, "_",susp_long$variable)
-susp_long$variable <- NULL
-susp_wide <- dcast(susp_long, School_Year + School_Code + Student_Group ~ Metric, value.var = "value")
-colnames(susp_wide) <- c("year","school_code","subgroup","state_suspended_1","suspended_1_n","suspended_1","state_suspended_11","suspended_11_n","suspended_11","state_incidents","incidents_n","incidents")
-susp_wide$suspended_11_n <- NULL
-susp_wide$suspended_1_n <- NULL
-susp_wide$incidents_n <- NULL
-susp_wide$state_incidents <- NULL
-susp_wide$state_suspended_1 <- susp_wide$state_suspended_1/100
-susp_wide$suspended_1 <- susp_wide$suspended_1/100
-susp_wide$state_suspended_11 <- susp_wide$state_suspended_11/100
-susp_wide$suspended_11 <- susp_wide$suspended_11/100
-susp_wide <- select(susp_wide, school_code, year,subgroup, suspended_1, suspended_11, state_suspended_1, state_suspended_11, incidents)
-susp_wide$school_code <- sapply(susp_wide$school_code, leadgr, 4)
-
-##
 disc <- sqlQuery(dbrepcard_prod,"select * from equity_report_school_longitudinal where reported=1 and metric in('Total Expulsions','Expulsion Rate','Suspended 1+','Suspended 11+','Total Suspensions')") %>% mutate(lea_code=sapply(lea_code,leadgr,4),school_code=sapply(school_code,leadgr,4),subgroup=ifelse(subgroup %in% c('Male','Female'),toupper(subgroup),subgroup)) %>% select(-(school_year),-(lea_code),-(school_name),-(lea_name),-(reported),-(reported),-(reason_not_reported),-(grade),-(enrollment),-(month))
 susp <- disc %>% filter(metric %in% c('Suspended 1+','Suspended 11+','Total Suspensions'))
 susp_long <- melt(susp, id.vars = c("year","school_code","subgroup","metric"))
@@ -35,10 +15,10 @@ suspw$suspended_11_n <- NULL
 suspw$suspended_1_n <- NULL
 suspw$incidents_n <- NULL
 suspw$state_incidents <- NULL
-suspw$state_suspended_1 <- suspw$state_suspended_1/100
-# suspw$suspended_1 <- suspw$suspended_1/100
-suspw$state_suspended_11 <- suspw$state_suspended_11/100
-# suspw$suspended_11 <- suspw$suspended_11/100
+suspw$suspended_1 <- round(suspw$suspended_1/100,3)
+suspw$suspended_11 <- round(suspw$suspended_11/100,4)
+suspw$state_suspended_1 <- round(suspw$state_suspended_1/100,3)
+suspw$state_suspended_11 <- round(suspw$state_suspended_11/100,4)
 suspend <- select(suspw,school_code,year,subgroup,suspended_1,suspended_11,state_suspended_1,state_suspended_11,incidents)
 
 strtable(suspend)

Equity Report Exhibit/Suspension JSON ETL/state_etl.R
@@ -4,53 +4,6 @@ library(jsonlite)
 library(reshape2)
 library(dplyr)
 
-# susp <- sqlQuery(dbrepcard_prod, "SELECT [School_Code], [School_Year], [Student_Group], [Metric], [NSize], [SchoolScore], [AverageScore]
-#   	FROM [dbo].[equity_longitudinal3] WHERE [Metric] in ('Suspended 1+','Suspended 11+','Total Suspensions')")
-# susp <- subset(susp, !is.na(School_Code))
-
-# susp_long <- melt(susp, id.vars = c("School_Year", "School_Code", "Student_Group", "Metric"))
-# susp_long$Metric<- paste0(susp_long$Metric, "_",susp_long$variable)
-# susp_long$variable <- NULL
-
-# susp_wide <- dcast(susp_long, School_Year + School_Code + Student_Group ~ Metric, value.var = "value")
-# colnames(susp_wide) <- c("year","school_code","subgroup","state_suspended_1","suspended_1_n","suspended_1","state_suspended_11","suspended_11_n","suspended_11","state_incidents","incidents_n","incidents")
-# susp_wide$suspended_11_n <- NULL
-# susp_wide$suspended_1_n <- NULL
-# susp_wide$incidents_n <- NULL
-# susp_wide$state_incidents <- NULL
-# susp_wide$state_suspended_1 <- susp_wide$state_suspended_1/100
-# susp_wide$suspended_1 <- susp_wide$suspended_1/100
-# susp_wide$state_suspended_11 <- susp_wide$state_suspended_11/100
-# susp_wide$suspended_11 <- susp_wide$suspended_11/100
-# susp_wide <- select(susp_wide, school_code, year,subgroup, suspended_1, suspended_11, state_suspended_1, state_suspended_11, incidents)
-# susp_wide$school_code <- sapply(susp_wide$school_code, leadgr, 4)
-
-# state_vals <- susp_wide[which(susp_wide$school_code=='0161'),]
-# state_vals$suspended_1 <- state_vals$state_suspended_1
-# state_vals$suspended_11 <- state_vals$state_suspended_11
-# state_vals$incidents <- state_vals$incidents
-
-# state_susp_all <- state_vals %>%
-#   mutate(population='All',state_suspended_1=NA,state_suspended_11=NA,state_incidents=NA) %>%
-#   select(year,subgroup,population,suspended_1,suspended_11,incidents,
-#   	state_suspended_1,state_suspended_11,state_incidents)
-
-# state_susp_gen <- state_vals %>%
-#   mutate(population='Gen',suspended_1=round(suspended_1/1.25,4),
-#          suspended_11=round(suspended_11/1.25,4),incidents=round(incidents/1.25,0),
-#          state_suspended_1=NA,state_suspended_11=NA,state_incidents=NA) %>%
-#   select(year,subgroup,population,suspended_1,suspended_11,incidents,
-#   	state_suspended_1,state_suspended_11,state_incidents)
-
-# state_susp_alt <- state_vals %>%
-#   mutate(population='Alt',suspended_1=round(suspended_1/1.75,4),
-#          suspended_11=round(suspended_11/1.75,4),incidents=round(incidents/1.75,0),
-#          state_suspended_1=NA,state_suspended_11=NA,state_incidents=NA) %>%
-#   select(year,subgroup,population,suspended_1,suspended_11,incidents,
-#   	state_suspended_1,state_suspended_11,state_incidents)
-
-# state_susp <- rbind.data.frame(state_susp_all,state_susp_gen,state_susp_alt)
-
 susp <- sqlQuery(dbrepcard_prod,"select * from equity_report_state_longitudinal where reported=1 and metric in ('Suspended 1+','Suspended 11+','Total Suspensions')") %>%
 mutate(subgroup=ifelse(subgroup %in% c('Male','Female'),toupper(subgroup),subgroup),population='All') %>% select(-starts_with("days"),-(school_year),-(grade),-starts_with("mo"),-starts_with("re"),-(count),-(nsize),-(enrollment))
 
@@ -59,6 +12,8 @@ susp_long$metric<- paste0(susp_long$metric, "_",susp_long$variable)
 susp_long$variable <- NULL
 susp_wide <- dcast(susp_long, year + subgroup +population ~ metric, value.var = "value")
 colnames(susp_wide) <- c("year","subgroup","population","suspended_1","suspended_11","incidents")
+susp_wide$suspended_1 <- round(susp_wide$suspended_1,3)
+susp_wide$suspended_11 <- round(susp_wide$suspended_11,4)
 susp_wide$state_suspended_1 <- NA
 susp_wide$state_suspended_11 <- NA
 susp_wide$state_incidents <- NA

commit 586e638270de7c4be20a04ae4be553c6aa15244f

Code: Fixing data error introduced in commit 2ac05c4. School suspension rates were being divided by 100 before rounding like average values.

Equity Report Exhibit/Suspension JSON ETL/school_etl.R
@@ -15,8 +15,8 @@ suspw$suspended_11_n <- NULL
 suspw$suspended_1_n <- NULL
 suspw$incidents_n <- NULL
 suspw$state_incidents <- NULL
-suspw$suspended_1 <- round(suspw$suspended_1/100,3)
-suspw$suspended_11 <- round(suspw$suspended_11/100,4)
+suspw$suspended_1 <- round(suspw$suspended_1,3)
+suspw$suspended_11 <- round(suspw$suspended_11,4)
 suspw$state_suspended_1 <- round(suspw$state_suspended_1/100,3)
 suspw$state_suspended_11 <- round(suspw$state_suspended_11/100,4)
 suspend <- select(suspw,school_code,year,subgroup,suspended_1,suspended_11,state_suspended_1,state_suspended_11,incidents)