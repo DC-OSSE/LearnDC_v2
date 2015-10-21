097737f6ce9425ab8222bbe54a1a1d77e3b7fff0
aaditya-madala <aaditya.madala@gmail.com>
2015-10-21 17:13:13 -0400

    Code: Updating School Suspension ETL to account for changes in data as of 10/21/2015.

--- Equity Report Exhibit/Suspension JSON ETL/school_etl.R
+++ Equity Report Exhibit/Suspension JSON ETL/school_etl.R
@@ -17,8 +17,8 @@ suspw$incidents_n <- NULL
 suspw$state_incidents <- NULL
 suspw$suspended_1 <- round(suspw$suspended_1,3)
 suspw$suspended_11 <- round(suspw$suspended_11,4)
-suspw$state_suspended_1 <- round(suspw$state_suspended_1/100,3)
-suspw$state_suspended_11 <- round(suspw$state_suspended_11/100,4)
+suspw$state_suspended_1 <- round(suspw$state_suspended_1,3)
+suspw$state_suspended_11 <- round(suspw$state_suspended_11,4)
 suspend <- select(suspw,school_code,year,subgroup,suspended_1,suspended_11,state_suspended_1,state_suspended_11,incidents)
 
 strtable(suspend)
