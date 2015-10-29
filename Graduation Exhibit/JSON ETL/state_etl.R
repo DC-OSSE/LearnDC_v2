3c6c13523d831f74435ad47b7b6475e4125e0544
aaditya-madala <aaditya.madala@gmail.com>
2015-10-29 17:02:17 -0400

    Code: removing dummy alt and gen population code and data

--- Graduation Exhibit/JSON ETL/state_etl.R
+++ Graduation Exhibit/JSON ETL/state_etl.R
@@ -9,27 +9,15 @@ state_grad <- subset(state_grad, cohort_size >= 25 & !is.na(graduates))
 
 state_grad$population <- "All"
 
-state_grad_gen <- state_grad
-state_grad_gen$population <- "Gen"
-state_grad_gen$graduates <- round(state_grad_gen$graduates * .75,0)
-state_grad_gen$cohort_size <- round(state_grad_gen$cohort_size * .75,0)
-
-state_grad_alt <- state_grad
-state_grad_alt$population <- "Alt"
-state_grad_alt$graduates <- round(state_grad_gen$graduates * .25,0)
-state_grad_alt$cohort_size <- round(state_grad_gen$cohort_size * .25,0)
-
-state_grad_all <- rbind.data.frame(state_grad,state_grad_gen,state_grad_alt)
-
-strtable(state_grad_all)
+strtable(state_grad)
 
 key_index <- c(1:3,6)
 value_index <- c(4,5)
 
 
-nested_list <- lapply(1:nrow(state_grad_all), FUN = function(i){ 
-  list(key = list(state_grad_all[i,key_index]), 
-       val = list(state_grad_all[i,value_index]))
+nested_list <- lapply(1:nrow(state_grad), FUN = function(i){ 
+  list(key = list(state_grad[i,key_index]), 
+       val = list(state_grad[i,value_index]))
 })
 
 json <- toJSON(nested_list, na="null")
