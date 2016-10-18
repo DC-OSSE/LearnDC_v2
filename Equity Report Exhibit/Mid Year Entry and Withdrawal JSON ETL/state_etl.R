35b63c30133a943361dde8e79cf21885b0ac798e
aaditya-madala <aaditya.madala@gmail.com>
2016-10-18 18:45:38 -0400

    Code/Data: updating state enr and mye/myw to be similar to school equity code.

--- Equity Report Exhibit/Mid Year Entry and Withdrawal JSON ETL/state_etl.R
+++ Equity Report Exhibit/Mid Year Entry and Withdrawal JSON ETL/state_etl.R
@@ -3,13 +3,13 @@ library(jsonlite)
 library(reshape2)
 library(dplyr)
 
-myew <- sqlQuery(dbrepcard_prod,"select * from equity_report_state_longitudinal where school_year='2015-2016' and reported=1 and metric in ('Entry','Exit','Net Cumulative')") %>% mutate(month=ifelse(month %in% 'October',10,ifelse(month %in% 'November',11,ifelse(month %in% 'December',12,ifelse(month %in% 'January',1,ifelse(month %in% 'February',2,ifelse(month %in% 'March',3,ifelse(month %in% 'April',4,ifelse(month %in% 'May',5,NA))))))))) %>% arrange(month_order) %>% select(-starts_with("days"),-(school_year),-(grade),-(month_order),-starts_with("re"),-starts_with("days"),-(count),-(nsize),-(enrollment))
+myew <- sqlQuery(dbrepcard_prod,"select * from equity_report_state_longitudinal where school_year='2015-2016' and reported=1 and metric in ('Withdrawal','Entry','Net Cumulative')") %>% mutate(month=ifelse(month %in% 'October',10,ifelse(month %in% 'November',11,ifelse(month %in% 'December',12,ifelse(month %in% 'January',1,ifelse(month %in% 'February',2,ifelse(month %in% 'March',3,ifelse(month %in% 'April',4,ifelse(month %in% 'May',5,NA))))))))) %>% arrange(month_order) %>% select(-starts_with("days"),-(school_year),-(grade),-(month_order),-starts_with("re"),-starts_with("days"),-(count),-(nsize),-(enrollment))
 
 move_long <- melt(myew,id.vars=c("year","subgroup","month","metric","population"))
 move_long$metric <- paste0(move_long$metric,"_",move_long$variable)
 move_long$variable <- NULL
 move_wide <- dcast(move_long,year + subgroup + month + population ~ metric,value.var="value")
-colnames(move_wide) <- c("year","subgroup","month","population","entry","withdrawal","net_cumulative")
+colnames(move_wide) <- c("year","subgroup","month","population","entry","net_cumulative","withdrawal")
 move_wide$withdrawal <- round(move_wide$withdrawal,3)
 move_wide$entry <- round(move_wide$entry,3)
 move_wide$net_cumulative <- round(move_wide$net_cumulative,3)
