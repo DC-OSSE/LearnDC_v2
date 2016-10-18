4dd96cfcbe77d16ac679a0d691e5ff1aff387732
aaditya-madala <aaditya.madala@gmail.com>
2016-10-18 18:06:27 -0400

    Code: Updating school suspension equity for sy1516

--- Equity Report Exhibit/Suspension JSON ETL/school_etl.R
+++ Equity Report Exhibit/Suspension JSON ETL/school_etl.R
@@ -1,10 +1,9 @@
-setwd("U:/LearnDC ETL V2/Equity Report Exhibit/Suspension JSON ETL")
-source("U:/R/tomkit.R")
+source(paste(root_dir,'imports/helpers.R',sep=''))
 library(jsonlite)
 library(reshape2)
 library(dplyr)
 
-disc <- sqlQuery(dbrepcard_prod,"select * from equity_report_school_longitudinal where reported=1 and metric in('Total Expulsions','Expulsion Rate','Suspended 1+','Suspended 11+','Total Suspensions')") %>% mutate(lea_code=sapply(lea_code,leadgr,4),school_code=sapply(school_code,leadgr,4),subgroup=ifelse(subgroup %in% c('Male','Female'),toupper(subgroup),subgroup)) %>% select(-(school_year),-(lea_code),-(school_name),-(lea_name),-(reported),-(reported),-(reason_not_reported),-(grade),-(enrollment),-(month))
+disc <- sqlQuery(dbrepcard_prod,"select * from equity_report_school_longitudinal where school_year = '2015-2016' and reported=1 and metric in('Total Expulsions','Expulsion Rate','Suspended 1+','Suspended 11+','Total Suspensions')") %>% mutate(lea_code=sapply(lea_code,leadgr,4),school_code=sapply(school_code,leadgr,4),subgroup=ifelse(subgroup %in% c('Male','Female'),toupper(subgroup),subgroup)) %>% select(-(school_year),-(lea_code),-(school_name),-(lea_name),-(reported),-(reported),-(reason_not_reported),-(grade),-(enrollment),-(month))
 susp <- disc %>% filter(metric %in% c('Suspended 1+','Suspended 11+','Total Suspensions'))
 susp_long <- melt(susp, id.vars = c("year","school_code","subgroup","metric"))
 susp_long$metric<- paste0(susp_long$metric, "_",susp_long$variable)
@@ -19,6 +18,7 @@ suspw$suspended_1 <- round(suspw$suspended_1,3)
 suspw$suspended_11 <- round(suspw$suspended_11,4)
 suspw$state_suspended_1 <- round(suspw$state_suspended_1,3)
 suspw$state_suspended_11 <- round(suspw$state_suspended_11,4)
+suspw$incidents <- ifelse(is.na(suspw$incidents), 0, suspw$incidents)
 suspend <- select(suspw,school_code,year,subgroup,suspended_1,suspended_11,state_suspended_1,state_suspended_11,incidents)
 
 strtable(suspend)
@@ -28,7 +28,7 @@ value_index <- 4:8
 num_orphans <- 0
 
 for(i in unique(suspend$school_code)){
-	setwd("U:/LearnDC ETL V2/Export/JSON/school")
+	setwd(paste(root_dir, 'Export/JSON/school', sep=''))
 
 	if(file.exists(i)){
 	    setwd(file.path(i))
