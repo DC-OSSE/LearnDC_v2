7558ebc03a56114b267e0cd9472269cf01e3adc2
aaditya-madala <aaditya.madala@gmail.com>
2016-11-09 10:24:47 -0500

    Code: updating school grad to remove graduates/cohort_size and include subgroup=all acgr rates

--- Graduation Exhibit/JSON ETL/school_etl.R
+++ Graduation Exhibit/JSON ETL/school_etl.R
@@ -1,9 +1,11 @@
 source(paste(root_dir,'imports/helpers.R',sep=''))
 library(jsonlite)
 
-school_grad <- sqlQuery(dbrepcard_prod, "SELECT * FROM [dbo].[graduation_school_exhibit] where reported = 1 and cohort_size >= 25 and isnull(graduates,'')!=''")
+school_grad <- sqlQuery(dbrepcard_prod, "SELECT * FROM [dbo].[graduation_school_exhibit] where (reported = 1 and cohort_size >= 25 and isnull(graduates,'')!='') or (isnull(graduates,'')!='' and subgroup = 'All')")
 school_grad$school_code <- sapply(school_grad$school_code, leadgr, 4)
 school_grad$acgr <- round(school_grad$graduates/school_grad$cohort_size,3)
+school_grad$graduates <- NA
+school_grad$cohort_size <- NA
 
 key_index <- c(5,6,7)
 value_index <- c(8,9,12)
