344d27acc5cc2bd4700925e2d2251d3cd1a58f9f
aaditya-madala <aaditya.madala@gmail.com>
2016-11-09 21:50:08 -0500

    Updating Grad to add acgr/suppression in val json. Sector

--- Graduation Exhibit/JSON ETL/sector_etl.R
+++ Graduation Exhibit/JSON ETL/sector_etl.R
@@ -6,13 +6,14 @@ sector_grad <- sqlQuery(dbrepcard_prod, "SELECT * FROM [dbo].[graduation_sector_
 sector_grad <- subset(sector_grad, lea_code==0)
 sector_grad$lea_code <- sapply(sector_grad$lea_code, leadgr, 4)
 sector_grad$acgr <- round(sector_grad$graduates/sector_grad$cohort_size,3)
-sector_grad$acgr <- ifelse(sector_grad$reported == 0 & sector_grad$reason_not_reported == 'n<25','n<25',sector_grad$acgr)
+sector_grad$acgr <- ifelse(sector_grad$reported == 0 & sector_grad$reason_not_reported == 'n<25',NA,sector_grad$acgr)
+sector_grad$suppressed <-ifelse(sector_grad$reported == 0 & sector_grad$reason_not_reported == 'n<25',1,0)
 sector_grad$graduates <- NA
 sector_grad$cohort_size <- NA
 
 
 key_index <- c(3,4,5)
-value_index <- c(10)
+value_index <- c(10,11)
 num_orphans <- 0
 
 
